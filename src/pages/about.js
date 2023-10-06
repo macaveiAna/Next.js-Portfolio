1a505b616cf3006589a4b620fc83ccd1ddaf81dd
macaveiAna <83480446+macaveiAna@users.noreply.github.com>
2023-10-05 23:21:53 -0600

    added skills

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,15 +1,42 @@
 import AnimatedText from '@/components/AnimatedText'
 import Layout from '@/components/Layout'
 import Head from 'next/head'
-import React from 'react'
+import { useInView, useMotionValue, useSpring } from 'framer-motion'
+import React, { useEffect, useRef } from 'react'
 import Image from 'next/image'
 import profilePic from "../../public/images/profile/developer-pic-7.png";
+import Skills from '@/components/Skills'
+
+
+const AnimatedNumbers = ({value}) => {
+const ref = useRef(null);
+const motionValue = useMotionValue(0);
+const springValue = useSpring(motionValue, { duration: 3000 });
+const isInView = useInView(ref, {once:true});
+
+useEffect(() => {
+    if(isInView){
+        motionValue.set(value);
+    }
+}, [isInView, value, motionValue])
+
+useEffect(() => {
+    springValue.on("change", (latest) => {
+        if(ref.current && latest.toFixed(0) <= value){
+            ref.current.textContent = latest.toFixed(0);
+        }
+    })
+
+}, [springValue, value])
+
+    return <span>ref={ref}</span>
+}
 
 const about = () => {
     return(
         <>
             <Head>
-                <title>CodeBucks | About Page</title>
+                <title>Adriana | About Page</title>
                 <meta name="description" content="any description"/>
             </Head>
             <main className='flex w-full flex-col items-center justify-center'>
@@ -51,9 +78,22 @@ const about = () => {
 <div className='col-span-3 relative h-max rounded-2xl barder-2 border-solid border-dark
 bg-light p-8
 '>
+<div className='absolute top-0 -right-3 -z10 w-[102] h-[103] rounded-[2rem] bg-dark'/>
     <Image src={profilePic} alt="Adriana" className='w-full h-auto rounded-2xl'/>
 </div>
-                </div>
+
+<div className='col-span-2 flex flex-col items-end justify-between'>
+    
+    <div className='flex flex-col items-end justify-center'>
+        <span className='inline-block text-7xl font-bold'>
+        4+
+        </span>
+        <h2 className='text-xl font-medium capitalize text-dark/75'>years of experience</h2>
+    </div>
+</div>
+             
+                 </div>
+                 <Skills />
                 </Layout>
             </main>
         </>
