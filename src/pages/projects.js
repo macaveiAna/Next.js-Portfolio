afa794b6cda2d56d7a8b260a567d290c55f763c3
macaveiAna <83480446+macaveiAna@users.noreply.github.com>
2023-10-11 13:53:00 -0600

    added MovingImg

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -6,6 +6,9 @@ import Link from 'next/link'
 import Image from 'next/image'
 import { GithubIcon } from '@/components/Icons'
 import project1 from "../../public/images/projects/portfolio-cover-image2.png";
+import {motion} from "framer-motion"
+
+const FramerImage = motion(Image);
 
 const FeaturedProject = ({type, title, summary, img, link, github}) => {
     return(
@@ -19,7 +22,10 @@ const FeaturedProject = ({type, title, summary, img, link, github}) => {
         target="_blank"
         className='w-1/2 cursor-pointer overflow-hidden rounded-lg'
         >
-            <Image src={img} alt={title} className="w-full h-auto" />
+            <FramerImage src={img} alt={title} className="w-full h-auto" 
+            whileHover={{scale:1.05}}
+            transition={{duration:0.2}}
+            />
               
         </Link>
         <div className='w-1/2 flex flex-col items-start justify-between pl-6'>
@@ -56,7 +62,10 @@ const Project = ({title, type, img, link, github}) => {
         target="_blank"
         className='w-full cursor-pointer overflow-hidden rounded-lg'
         >
-            <Image src={img} alt={title} className="w-full h-auto" />
+            <FramerImage src={img} alt={title} className="w-full h-auto"
+            whileHover={{scale:1.05}}
+            transition={{duration:0.2}}
+            />
               
         </Link>
         <div className='w-full flex flex-col items-start justify-between mt-4'>
