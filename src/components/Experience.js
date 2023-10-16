fd662decfea82caa63eda8343289bdc1f0b1e460
macaveiAna <83480446+macaveiAna@users.noreply.github.com>
2023-10-16 11:42:23 -0600

    changes

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -40,13 +40,15 @@ const Experience = () => {
                 Experience
             </h2>
 
-            <div ref={ref} className='w-[75%] mx-auto relative'>
+            <div ref={ref} className='w-[75%] mx-auto relative lg:w-[90%] md:w-full'>
 
         <motion.div 
         style={{scaleY: scrollYProgress}}
-            className='absolute left-9 top-0 w-[4px] h-full bg-dark origin-top dark:bg-light'/>
+            className='absolute left-9 top-0 w-[4px] h-full bg-dark origin-top dark:bg-light
+            md:w-[2px] md:left-[30px] xs:left-[20px]
+            '/>
 
-                <u1 className="w-full flex flex-col items-start justify-between ml-4">
+                <u1 className="w-full flex flex-col items-start justify-between ml-4 xs:ml-2">
                     <Details 
                     position="Software Engineer Intern" company="LegitScript"
                     companyLink="https://www.legitscript.com/"
