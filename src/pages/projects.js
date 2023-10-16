4b28fdeb69014d3bcca40f4705132703aebefee7
macaveiAna <83480446+macaveiAna@users.noreply.github.com>
2023-10-16 13:05:11 -0600

    new commit

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -14,13 +14,14 @@ const FeaturedProject = ({type, title, summary, img, link, github}) => {
     return(
         <article className='w-full flex items-center justify-between relative rounded-br-2xl
         rounded-3xl border border-solid border-dark bg-light shadow-2xl p-12 dark:bg-dark dark:border-light
+        lg:flex-col lg:p-8 xs:rounded-2xl xs:rounded-br-3xl xs:p-4
         '>
         <div className='absolute top-0 -right-3 -z-10 w-[101%] h-[103%] rounded-[2.5rem] bg-dark dark:bg-light
         rounded-br-3xl
         '/>
         <Link href={link} 
         target="_blank"
-        className='w-1/2 cursor-pointer overflow-hidden rounded-lg'
+        className='w-1/2 cursor-pointer overflow-hidden rounded-lg lg:w-full'
         >
             <FramerImage src={img} alt={title} className="w-full h-auto" 
             whileHover={{scale:1.05}}
@@ -35,10 +36,10 @@ const FeaturedProject = ({type, title, summary, img, link, github}) => {
             />
               
         </Link>
-        <div className='w-1/2 flex flex-col items-start justify-between pl-6'>
-            <span className='text-primary font-medium text-xl dark:text-primaryDark'>{type}</span>
+        <div className='w-1/2 flex flex-col items-start justify-between pl-6 lg:w-full lg:pl-0 lg:pt-6'>
+            <span className='text-primary font-medium text-xl dark:text-primaryDark xs:text-base'>{type}</span>
             <Link href={link} target="_blank" className='hover:underline underline-offset-2'>
-            <h2 className='my-2 w-full text-left text-4xl font-bold dark:text-light'>{title}</h2>              
+            <h2 className='my-2 w-full text-left text-4xl font-bold dark:text-light sm:text-sm'>{title}</h2>              
         </Link>
        
        <p className='my-2 font-medium text-dark dark:text-light'>{summary}</p>
@@ -49,6 +50,7 @@ const FeaturedProject = ({type, title, summary, img, link, github}) => {
         target="_blank" 
         className='ml-4 rounded-lg bg-dark text-light p-2 px-6 text-lg font-semibold
         dark:bg-light dark:text-dark
+        sm:px-4 sm:text-base
         '
         > Visit Project 
         </Link>
@@ -117,10 +119,10 @@ const projects = ({ title, type, img, link, github }) => {
         <Layout className='pt-16'>
             <AnimatedText text ="Imagination Inpires Knowledge" 
             
-            className='mb-16'
+            className='mb-16 lg:!text-7xl sm:mb-8 sm:!text-6xl xs:!text-4xl'
             />
 
-            <div className='grid grid-cols-12 gap-24 gap-y-32'>
+            <div className='grid grid-cols-12 gap-24 gap-y-32 xl:gap-x-16 lg:gap-x-8 md:gap-y-24 sm:gap-x-0'>
                 <div className='col-span-12'>
                 <FeaturedProject
                 title="React Portfolio Website"
