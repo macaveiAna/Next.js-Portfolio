30ba93f4a5916a28cbec3dafad6e6a542fd6a3d1
macaveiAna <83480446+macaveiAna@users.noreply.github.com>
2023-10-15 21:27:00 -0600

    fixed projects dark mode

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -13,9 +13,9 @@ const FramerImage = motion(Image);
 const FeaturedProject = ({type, title, summary, img, link, github}) => {
     return(
         <article className='w-full flex items-center justify-between relative rounded-br-2xl
-        rounded-3xl border border-solid border-dark bg-light shadow-2xl p-12
+        rounded-3xl border border-solid border-dark bg-light shadow-2xl p-12 dark:bg-dark dark:border-light
         '>
-        <div className='absolute top-0 -right-3 -z-10 w-[101%] h-[103%] rounded-[2.5rem] bg-dark
+        <div className='absolute top-0 -right-3 -z-10 w-[101%] h-[103%] rounded-[2.5rem] bg-dark dark:bg-light
         rounded-br-3xl
         '/>
         <Link href={link} 
@@ -29,12 +29,12 @@ const FeaturedProject = ({type, title, summary, img, link, github}) => {
               
         </Link>
         <div className='w-1/2 flex flex-col items-start justify-between pl-6'>
-            <span className='text-primary font-medium text-xl'>{type}</span>
+            <span className='text-primary font-medium text-xl dark:text-primaryDark'>{type}</span>
             <Link href={link} target="_blank" className='hover:underline underline-offset-2'>
             <h2 className='my-2 w-full text-left text-4xl font-bold'>{title}</h2>              
         </Link>
        
-       <p className='my-2 font-medium text-dark'>{summary}</p>
+       <p className='my-2 font-medium text-dark dark:text-ligth'>{summary}</p>
       
         <div className='mt-2 flex items-center'>
         <Link href={github} target="_blank" className='w-10'> <GithubIcon /> </Link>
@@ -52,7 +52,7 @@ const Project = ({title, type, img, link, github}) => {
     return(
 
         <article className='w-full flex flex-col item-center justify-center rounded-2xl
-        border border-colid border-dark bg-light p-6 relative
+        border border-colid border-dark bg-light p-6 relative dark:bg-dark
         '>
 
 <div className='absolute top-0 -right-3 -z-10 w-[101%] h-[103%] rounded-[2rem] bg-dark
