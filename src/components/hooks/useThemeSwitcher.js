011803a60201c93fda8fc3555de8f1102962fb86
macaveiAna <83480446+macaveiAna@users.noreply.github.com>
2023-10-15 21:09:34 -0600

    fixed aboutme page

--- src/components/hooks/useThemeSwitcher.js
+++ src/components/hooks/useThemeSwitcher.js
@@ -34,6 +34,8 @@ const useThemeSwitcher = () => {
             }
         }
 
+        handleChange();
+
         mediaQuery.addEventListener("change", handleChange)
 
         return () => mediaQuery.removeEventListener("change", handleChange)
