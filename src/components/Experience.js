5d9a6fb188b448b4d56eea5a997a892b656e6a30
macaveiAna <83480446+macaveiAna@users.noreply.github.com>
2023-10-16 11:36:14 -0600

    new changes

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -4,7 +4,7 @@ import LiIcon from './LiIcon'
 
 const Details =  ({position, company, companyLink, time, address, work}) => {
     const ref = useRef(null);
-    return <li ref={ref} className='my-8 first:mt-0 last:mb-0 w-[60%] mx-auto flex flex-col items-center justify-between'>
+    return <li ref={ref} className='my-8 first:mt-0 last:mb-0 w-[60%] mx-auto flex flex-col items-center justify-between md:w-[80%]'>
 
         <LiIcon reference={ref}/>
         <motion.div
@@ -12,14 +12,14 @@ const Details =  ({position, company, companyLink, time, address, work}) => {
         whileInView={{y:0}}
         transition={{duration:0.5, type:"spring"}}
         >
-            <h3 className='capitalize font-bold text-2xl'>{position}&nbsp;<a href={companyLink}
+            <h3 className='capitalize font-bold text-2xl sm:text-xl xs:text-lg'>{position}&nbsp;<a href={companyLink}
             target="_blank"
             className='text-primary dark:text-primaryDark capitalize'
             >{company}</a></h3>
-            <span className='capitalize font-medium text-dark/75 dark:text-light/75'>
+            <span className='capitalize font-medium text-dark/75 dark:text-light/75 xs:text-sm'>
                 {time} | {address}
             </span>
-            <p className='font-medium w-full'>
+            <p className='font-medium w-full md:text-sm'>
                 {work}
             </p>
         </motion.div>
@@ -36,7 +36,7 @@ const Experience = () => {
     )
     return (
         <div className='my-64'>
-            <h2 className='font-bold text-8xl mb-32 w-full text-center'>
+            <h2 className='font-bold text-8xl mb-32 w-full text-center md:text-6xl xs:text-4xl md:mb-16'>
                 Experience
             </h2>
 
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -26,6 +26,10 @@ const Skills = () => {
             <h2 className='font-bold text-8xl mt-64 w-full text-center md:text-6xl md:mt-32'>Skills</h2>
             <div className='w-full h-screen relative flex items-center justify-center rounded-full bg-circularLight dark:bg-circularDark
             lg:h-[80vh] sm:h-[60vh] sx:h-[50vh]
+            lg:bg-circularLightLg lg:dark:bg-circularDarkLg
+            md:bg-circularLightMd md:dark:bg-circularDarkMd
+            sm:bg-circularLightSm sm:dark:bg-circularDarkSm
+
             '>
                 <motion.div className='flex items-center justify-center rounded-full font-semibold bg-dark text-light
                 p-8 shadow-dark cursor-pointer dark:text-dark dark:bg-light lg:p-6 md:p-4 xs:text-xs xs:p-2
