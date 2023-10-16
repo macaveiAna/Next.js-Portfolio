8cc935ca3b18072e6990eccc0e414ff9cc42dd32
macaveiAna <83480446+macaveiAna@users.noreply.github.com>
2023-10-15 21:57:00 -0600

    screen adaptability

--- src/pages/articles.js
+++ src/pages/articles.js
@@ -83,6 +83,10 @@ const FeateredArticle = ({img, title, time, summary, link}) => {
             <FramerImage src={img} alt={title} className="w-full h-auto" 
             whileHover={{scale:1.05}}
             transition={{duration:0.2}}
+            priority
+              sizes="(max-width: 768px) 100vw,
+              (max-width: 1200px) 50vw,
+              50vw"
             />
               
         </Link>
