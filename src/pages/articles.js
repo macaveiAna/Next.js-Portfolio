a960f63450a0e3ee4725ccb8728e0d6383a3e2d3
macaveiAna <83480446+macaveiAna@users.noreply.github.com>
2023-10-15 21:47:13 -0600

    changes

--- src/pages/articles.js
+++ src/pages/articles.js
@@ -60,10 +60,10 @@ const Article = ({img,title,date,link}) => {
         viewport={{once: true}}
         className='relative w-full p-4 py-6 my-4 rounded-xl flex items-center
         justiy-between bg-light text-dark first:mt-0 border border-solid border-dark
-        border-r-4 border-b-4
+        border-r-4 border-b-4 dark:border-light dark:bg-dark dark:text-light
         '>
             <MovingImg title={title} img={img} link={link} />
-            <span className='text-primary font-semibold pl-4'>{date}</span>
+            <span className='text-primary font-semibold pl-4 dark:text-primaryDark'>{date}</span>
         </motion.li>
     )
 
@@ -72,7 +72,7 @@ const Article = ({img,title,date,link}) => {
 const FeateredArticle = ({img, title, time, summary, link}) => {
 
     return(
-        <li className='relative col-span-1 w-full p-4 bg-light border border-solid border-dark rounded-2xl'>
+        <li className='relative col-span-1 w-full p-4 bg-light border border-solid border-dark rounded-2xl dark:bg-dark dark:border-light'>
         <div className='absolute top-0 -right-3 -z-10 w-[101%] h-[103%] rounded-[2rem] bg-dark
         rounded-br-3xl
         '/>
@@ -90,7 +90,7 @@ const FeateredArticle = ({img, title, time, summary, link}) => {
             <h2 className='capitalize text-2xl font-bold my-2 mt-4 hover:underline'>{title}</h2>
         </Link>
         <p className='text-sm mb-2'>{summary}</p>
-        <span className='text-primary font-semibold'>{time}</span>
+        <span className='text-primary font-semibold dark:text-primaryDark'>{time}</span>
         </li>
     )
 
@@ -103,7 +103,7 @@ const articles = () => {
                 <title>Adriana | Articles Page</title>
                 <meta name="description" content="any description"/>
             </Head>
-<main className='w-full mb-16 flex flex-col items items-center justify-center overflow-hidden'>
+<main className='w-full mb-16 flex flex-col items items-center justify-center overflow-hidden dark:text-light'>
     <Layout className='pt-16'>
         <AnimatedText text="Words Can Change The World! " className='mb-16'/>
         <ul className='grid grid-cols-2 gap-16'>
